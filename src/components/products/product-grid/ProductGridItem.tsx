9a28e4f7147698594e47d5481e3a59f130e41db2
Leandro Javier Martinez <leamartinez@santandertecnologia.com.ar>
2024-07-21 19:49:09 -0300

    Cambiar imagen - onMouseOver

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -1,7 +1,9 @@
+'use client';
+
 import { Product } from '@/interfaces'
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react'
+import React, { useState } from 'react'
 
 interface Props {
   product: Product;
@@ -10,18 +12,24 @@ interface Props {
 
 export const ProductGridItem = ({ product }: Props) => {
 
+const [ displayImage, setDisplayImage ] = useState( product.images[0])
 
   return (
     <div className='rounded-md overflow-hidden fade-in' >
+
       <Link href={`/product/${product.slug}`} >
-        <Image src={`/products/${product.images[0]}`} alt={product.title} width={500} height={500} className='w-full object-cover' />
+        <Image src={`/products/${ displayImage }`} alt={product.title} width={500} height={500} 
+          className='w-full object-cover rounded'
+          onMouseEnter={() => setDisplayImage(product.images[1])}
+          onMouseLeave={() => setDisplayImage(product.images[0])}
+        />
       </Link>
 
       <div className=' flex flex-col'>
-        <Link href={`/product/${product.slug}`} className='hover:text-blue-500'> {product.title} </Link>
+        <Link href={`/product/${ product.slug }`} className='hover:text-blue-500'> {product.title} </Link>
       </div>
 
-      <span className='font-bold'> ${product.price} </span>
+      <span className='font-bold'>${ product.price } </span>
 
     </div>
   )
