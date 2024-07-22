58b8c34b05e00a1c5703d73208e3ac3602beea51
Leandro Javier Martinez <leamartinez@santandertecnologia.com.ar>
2024-07-22 16:44:20 -0300

    Selector de tallas

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,10 +1,61 @@
 
-export default function ProductPage() {
+import { QuantitySelector, SizeSelector } from "@/components";
+import { titleFont } from "@/config/font";
+import { initialData } from "@/seed/seed";
+import { notFound } from "next/navigation";
+
+
+interface Props {
+  params: {
+    slug: string;
+  };
+
+}
+
+
+export default function ProductPage({ params }: Props) {
+
+  const { slug } = params;
+  const product = initialData.products.find(product => product.slug === slug);
+
+  if (!product) {
+    notFound();
+  }
 
 
   return (
-    <div>
-      <h1>Product Page</h1>
+    <div className="mt-5 mb-20 grid md:grid-cols-3 gap-3">
+
+      {/* Slideshow */}
+      <div className="col-span-1 md:col-span-2">
+
+      </div>
+
+      {/* Detalles */}
+      <div className="col-span-1 px-5">
+        <h1 className={`${titleFont.className} antialiased font-bold text-xl `}>
+          {product.title}
+        </h1>
+        <p className="text-lg mb-5"> ${product.price} </p>
+
+        {/* selector de talle */}
+        <SizeSelector selectedSize={ product.sizes[0] } availableSizes={ product.sizes } />
+
+        {/* selector de cantidad */}
+        <QuantitySelector quantity={ 1 } />
+
+        {/* BUTTON */}
+        <button className="btn-primary my-5"> Agregar al carrito </button>
+
+        {/* descripcion */}
+        <h3 className="font-bold text-sm"> Descripción </h3>
+        <p className="font-light"> {product.description} </p>
+
+      </div>
+
+
+
+
     </div>
   );
 }
