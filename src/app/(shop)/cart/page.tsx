4052fa0a734bcac4df3ee5fdc530f7d7ffeecbcc
Leandro Javier Martinez <leamartinez@santandertecnologia.com.ar>
2024-07-25 19:08:14 -0300

    pantalla de login

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -53,7 +53,7 @@ export default function CartPage() {
           </div>
 
           {/* Checkout - Resumen de la compra */}
-          <div className="bg-white rounded-xl shadow-xl p-7">
+          <div className="bg-white rounded-xl shadow-xl p-7  h-fit">
             <h2 className="text-2xl mb-2"> Resumen de orden </h2>
 
             <div className="grid grid-cols-2">
