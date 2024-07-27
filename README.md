56534541068c85f70b0bddff631af67feaf0a773
Leandro Javier Martinez <leamartinez@santandertecnologia.com.ar>
2024-07-26 22:54:39 -0300

    docker bd prisma

--- README.md
+++ README.md
@@ -19,7 +19,8 @@ SSR, SSG, CSR, ISR, Middlewares, Rutas dinámicas, Next API, Next Auth, Material
 [Zustand](https://zustand-demo.pmnd.rs/), 
 [clsx](https://nextjs.org/learn-pages-router/basics/assets-metadata-css/styling-tips), 
 [Swiper](https://swiperjs.com/), 
-
+[Prisma](https://www.prisma.io/)
+[ts-node] ```npm i -D ts-node``` //para poder ejecutar codigo typescript en node
 
 
 ## Getting Started
@@ -36,7 +37,9 @@ nvm use 18.17.0
 2. Crear una copia del ```.env.template```, renombrarlo a ```.ènv``` y cambiar las variables de entorno.
 3. Instalar las dependencias ```npm install```
 4. Levantar la base de datos ```docker compose up -d```
-5. Ejecutar el proyecto ```npm run dev```  - Ingresar a [http://localhost:3000](http://localhost:3000) 
+5. Correr las migraciones de Prisma ```npx prisma migrate dev``` 
+6. Ejecutar seed ```npm run seed```
+7. Ejecutar el proyecto ```npm run dev```  - Ingresar a [http://localhost:3000](http://localhost:3000) 
 
 
 
---
+++ src/seed/seed-database.ts
@@ -0,0 +1,87 @@
+import { initialData } from "./seed";
+import prisma from '../lib/prisma';
+
+async function main() {
+
+    // 1. borrar registros previos
+    // await Promise.all([
+        await prisma.productImage.deleteMany();
+        await prisma.product.deleteMany();
+        await prisma.category.deleteMany();
+
+    // ])
+
+    const { categories, products } = initialData;
+
+    // Categorias
+
+    const categoriesData = categories.map((name) => ({ name }));
+
+
+    await prisma.category.createMany({
+        data: categoriesData
+    });
+
+    const categoriesDb = await prisma.category.findMany();
+
+
+    const categoriesMap = categoriesDb.reduce((map, category) => {
+        map[category.name.toLowerCase()] = category.id;
+        return map;
+    }, {} as Record<string, string>); //<string= shirt, string=categoryID>
+
+
+
+    // Productos
+
+    products.forEach(async (product) => {
+        const { type, images, ...rest } = product;
+
+        const dbProduct = await prisma.product.create({
+            data: {
+                ...rest,
+                categoryId: categoriesMap[type]
+            }
+        })
+
+        // Images
+        const imagesData = images.map(image => ({
+            url: image,
+            productId: dbProduct.id
+        }))
+
+        await prisma.productImage.createMany({
+            data: imagesData
+        })
+
+
+
+
+    });
+
+
+
+    console.log('Seed ejecutado correctamente')
+}
+
+
+
+
+
+
+
+
+(() => {
+
+    if (process.env.NODE_ENV === 'production') return;
+
+    main();
+}
+
+)();
+
+// para poder ejecutar este archivo typescript en node, necesitamos instalar ts-node:
+// npm i ts-node
+// cd src/seed
+// para crear en esta carpeta el archivo tsconfig.json:
+// npx tsc --init
